b150b8de0e647ca7c2b38793bd81b1951aaf12a6
hverlin <hverlin@trifacta.com>
2020-03-26 23:00:07 +0100

    Add help dialog

--- public/javascripts/game/UITools.js
+++ public/javascripts/game/UITools.js
@@ -60,6 +60,13 @@ define(function () {
       _socket.emit('chat', '!grid '+ gridNumber);
     }
   }
+
+  function showHelpModal() {
+    const modal = document.getElementById("help-modal");
+    modal.style.display = "block";
+
+
+  }
   
   /*
   * Switch game screen to the selected view 
@@ -230,6 +237,31 @@ define(function () {
   */
   UITools.prototype.bindServerCommandButtons = function (socket) {
     _socket = socket;
+
+    const modal = document.getElementById("help-modal");
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
+
+    document.getElementById('gsc-command-help')
+      .addEventListener('click', showHelpModal, false);
+
+    document.getElementById('gsc-command-help')
+      .addEventListener('keydown', e => {
+        if(e.keyCode === 13) {
+          showHelpModal();
+        }
+      }, false);
 
     document.getElementById('gsc-command-grid')
       .addEventListener('click', changeGrid, false);
